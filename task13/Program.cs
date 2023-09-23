dd3fca149020572644e3c585ffe8fd5fad49be81
dev.ponomarevss <dev.ponomarevss@gmail.com>
2023-09-23 12:17:23 +0500

    task13 added

---
+++ task13/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+Console.Write("Input number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int thirdDigit = 0;
+if (a / 100 > 0)
+{
+    while (a > 100)
+    {
+        thirdDigit = a % 10;
+        a /= 10;  
+    }
+    Console.WriteLine($"The third digit is {thirdDigit}");
+}
+else
+{
+    Console.WriteLine("The number has no third digit.");
+}
