162d1d106e651c8b611139344c1c37cf105bb715
dev.ponomarevss <dev.ponomarevss@gmail.com>
2023-09-23 11:39:07 +0500

    task10 added

---
+++ task10/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+/**
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int num = new Random().Next(100, 1000);
+int secondDigit = num / 10 % 10;
+Console.WriteLine($"Second digit of {num} is {secondDigit}");
