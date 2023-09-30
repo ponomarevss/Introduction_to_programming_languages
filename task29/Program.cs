51835f8e83cd4bda91e41af38df9844837624804
dev.ponomarevss <dev.ponomarevss@gmail.com>
2023-09-30 23:45:19 +0500

    task29 added

---
+++ task29/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+/* 
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] 
+*/
+
+int[] GenerateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("\b\b]");
+}
+Console.Write("Generate an array with length: ");
+int length = Convert.ToInt32(Console.ReadLine());
+PrintArray(GenerateArray(length));
