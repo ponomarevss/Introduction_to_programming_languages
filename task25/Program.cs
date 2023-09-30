ad11c9832044d90fb9080e0119314299f3a08cfe
dev.ponomarevss <dev.ponomarevss@gmail.com>
2023-09-30 23:53:48 +0500

    task25 refactored

--- task25/Program.cs
+++ task25/Program.cs
@@ -3,13 +3,19 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 
 */
+
+int PowAToB(int a, int b)
+{
+    int res = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        res *= a;
+    }
+    return res;
+}
+
 Console.Write("Input number A: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number B: ");
 int b = Convert.ToInt32(Console.ReadLine());
-int res = 1;
-for (int i = 1; i <= b; i++)
-{
-    res *= a;
-}
-Console.WriteLine($"Result: {res}");
+Console.WriteLine($"Result: {PowAToB(a, b)}");
