4ce17d5472c236ff2762bc64c7a4fe8166b085eb
dev.ponomarevss <dev.ponomarevss@gmail.com>
2023-10-20 17:38:37 +0500

    task66 added

---
+++ task66/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 
+Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 
+*/
+
+int SumBetween(int m, int n)
+{
+    int min = Math.Min(m, n);
+    int max = Math.Max(m, n);
+
+    if (min == max) return min;
+    return min + SumBetween(min + 1, max);
+}
+
+Console.Write("Input integer M: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input integer N: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"SumBetween(M, N) = {SumBetween(a, b)}");
