9184b02e649741d1bac6f74096652833a94e17a1
dev.ponomarevss <dev.ponomarevss@gmail.com>
2023-10-20 18:13:09 +0500

    task68 added

---
+++ task68/Program.cs
@@ -0,0 +1,19 @@
+﻿/* 
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29 
+*/
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return Ackermann(m - 1, 1);
+    return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.Write("Input integer m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input integer n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ackermann({m}, {n}) = {Ackermann(m, n)}");
